86b89a5eab94bc7d37ed7513fce82aba8177c0d9
jgliner <jarrett.gliner@gmail.com>
2016-01-13 15:02:57 -0800

    Increment with enqueue and update queue size

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,7 +2,7 @@ var Queue = function() {
   var someInstance = {};
   var start = 0;
   var end = 0;
-  var queueSize = end - start;
+  var queueSize = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -10,6 +10,9 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[end] = value;
+    end++;
+    queueSize = end - start;
   };
 
   someInstance.dequeue = function() {
