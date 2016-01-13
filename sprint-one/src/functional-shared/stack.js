0b78720c69fc462e5f2f496efc7c660e44009c30
jgliner <jarrett.gliner@gmail.com>
2016-01-13 15:32:01 -0800

    Initialize structure, return size for stackMethods.size

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.len = 0;
+
+  _.extend(someInstance, Stack.stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+// _.extend = function(obj) {
+//     each(slice.call(arguments, 1), function(source) {
+//       if (source) {
+//         for (var prop in source) {
+//           obj[prop] = source[prop];
+//         }
+//       }
+//     });
+//     return obj;
+//   };
 
+//var stackMethods = {};
+
+Stack.stackMethods = {};
+
+Stack.stackMethods.push = function(val) {
+
+};
+
+Stack.stackMethods.pop = function() {
+  
+};
+
+Stack.stackMethods.size = function() {
+  return this.len;
+};
 
