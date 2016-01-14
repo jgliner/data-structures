be0e5e8c663cb294e328a87a7b73b81e7a0eaf77
jgliner <jarrett.gliner@gmail.com>
2016-01-14 10:11:15 -0800

    Initialize Stack constructor and methods for Pseudoclassical
    Always returns 0 for now

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.len = 0;
 };
 
+Stack.prototype.push = function(val) {
 
+};
+
+Stack.prototype.pop = function() {
+
+};
+
+Stack.prototype.size = function() {
+  return this.len;
+};
+
+var Collection = new Stack();
