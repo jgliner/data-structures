85132f84f80ba78ee0fee04c97238492e0f13191
jgliner <jarrett.gliner@gmail.com>
2016-01-14 10:02:39 -0800

    Initialize structure of queue.js (prototypal) and return zero for size

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.beg = 0;
+  someInstance.end = 0;
+  someInstance.len = 0;
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(val){
+
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+  return this.len;
+};
 
