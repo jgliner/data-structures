b12cd1c7bf5a3ce7727d5ccfc0731e4e84c6556d
jgliner <jarrett.gliner@gmail.com>
2016-01-14 11:14:25 -0800

    Implement list.addToTail and list.removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,10 +5,33 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     var newItem = new Node(value);
-    list.tail = newItem;
+    
+    // if no values
+    if (list.head === null) {
+      list.head = newItem;
+      list.tail = newItem;
+    }
+    // if one or more value
+    if (list.head === list.tail) {
+      list.tail.next = newItem;
+      list.tail = newItem;
+    }
   };
 
   list.removeHead = function() {
+    // if no values in LinkedList
+    if (list.head === null && list.tail === null) {
+      return;
+    }
+    // if one value in LinkedList
+    else if (list.head === list.tail) {
+      list.head = null;
+      list.tail = null;
+    }
+    // else
+    else {
+      list.head = list.head.next;
+    } 
   };
 
   list.contains = function(target) {
