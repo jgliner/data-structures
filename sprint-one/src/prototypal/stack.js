7fe898f7a5711afce2249cc94134491db1f714e6
jgliner <jarrett.gliner@gmail.com>
2016-01-13 16:36:59 -0800

    Initialize structure of Stack (prototypal)

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.len = 0;
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(val) {
 
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+  return this.len;
+};
