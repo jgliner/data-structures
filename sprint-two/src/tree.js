c70f916ccdab49433d1063432f10c6493bbb8269
jgliner <jarrett.gliner@gmail.com>
2016-01-14 20:09:41 -0800

    Complete tree class and helper methods
    All tests now pass.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -17,19 +17,29 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
   for (var i = 0; i < this.children.length; i++) {
     if (this.children[i].value === target) {
       return true;
     }
-    if (Array.isArray(this.children[i])) {
-      treeMethods(this.children[i]);
+    if (this.children[i].children.length > 0) {
+      for (var j = 0; j < this.children[i].children.length; j++) {
+        if (this.children[i].children[j].contains(target)) {
+          return true;
+        }
+      }
     }
   }
   return false;
 };
 
-
-
 /*
- * Complexity: What is the time complexity of the above functions?
+ *  Complexity: What is the time complexity of the above functions?
+
+    treeMethods.addChild = O(1) or constant
+
+    treeMethods.contains = O(n) or linear
+
  */
