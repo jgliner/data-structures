d1a10cad7a6ce59366e98e35d315a8aebf78260a
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-01-15 15:31:20 -0800

    Implement proper storage and retrieval methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,27 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var newTuple = new LimitedArray(2);
-  this._storage.set(index, newTuple);
-  //  if (this._storage.get(index) === undefined) {
-  //    this._storage.set(index, []);
-  //  }
-  // var temp = this._storage.get(index);
-  // temp.push([k,v]);
-  // this._storage.set(index,temp);
+  // var newTuple = new LimitedArray(2);
+  // this._storage.set(index, newTuple);
+   if (this._storage.get(index) === undefined) {
+     this._storage.set(index, []);
+   }
+  var temp = this._storage.get(index);
+  temp.push([k,v]);
+  this._storage.set(index,temp);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
-  this._storage.get(index).each(function(val) {
+  var temp = this._storage.get(index);
+  var found = false;
+  _.each(temp, function(val) {
     if (val[0] === k) {
-      return val[1];
+    console.log(val);
+      found = val[1];
     }
   });
-  return false;
 
-  //return this._storage.get(index);
+  return found;
 };
 
 HashTable.prototype.remove = function(k) {
