94f56f73f47eab8c330b45a00a0e18e859bcb877
jgliner <jarrett.gliner@gmail.com>
2016-01-15 14:30:59 -0800

    Initalize HashTable (store and retrieve value)

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,36 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+   if (this._storage.get(index) === undefined) {
+     this._storage.set(index, []);
+   }
+  var temp = this._storage.get(index);
+  temp.push([k,v]);
+  this._storage.set(index,temp);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  this._storage.get(index).each(function(val) {
+    if (val[0] === k) {
+      return val[1];
+    }
+  });
+  return false;
+
+  //return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage.get(index);
+
+  // this._storage[index].each(function(val, key, collection) {
+  //   if (val[0] === k) {
+  //     this._storage[index].splice(key, 1);
+  //   }
+  // });
 };
 
 
