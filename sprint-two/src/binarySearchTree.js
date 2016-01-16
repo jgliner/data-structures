619e6bbc2fe74ced8b9bebd5a4fe77fe409da295
jgliner <jarrett.gliner@gmail.com>
2016-01-15 21:02:38 -0800

    Initialize binarySearchTree class and helper methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,27 @@
 var BinarySearchTree = function(value) {
+
+  var someTree = Object.create(treeMethods);
+  someTree.val = value;
+  someTree.left = null;
+  someTree.right = null;
+  // someTree.leaves = {left: null, right: null};
+  return someTree;
+
 };
 
+var treeMethods = {};
+
+treeMethods.insert = function(val) {
+
+};
+
+treeMethods.contains = function(val) {
+  //returns boolean
+};
+
+treeMethods.depthFirstLog = function(cb) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
